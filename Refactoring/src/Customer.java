e59b8958ce2b92078ec7b229d1e341740e817796
Akhilesh.Prathuri <akhilesh.prathuri@servicenow.com>
2018-08-06 18:36:20 +0530

    Replaced local variable using Replace with temp query

--- Refactoring/src/Customer.java
+++ Refactoring/src/Customer.java
@@ -24,19 +24,16 @@ public class Customer {
 	   Enumeration rentals = _rentals.elements();
 	   String result = "Rental record for "+ getName() + "\n";
 	   while(rentals.hasMoreElements()){
-		   double thisAmount = 0;
-		   Rental each = (Rental) rentals.nextElement();
-		   
-		   thisAmount = each.getCharge();
-		   // add frequent rental points
+		   Rental each = (Rental) rentals.nextElement();   
+	      // add frequent rental points
 		   frequentRentalPoints++;
 		   // add bonus for two day new release rental
 		   if(each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1)
 			   frequentRentalPoints++;
 		   
 		   // show figures for this rental
-		   result+= "\t" + each.getMovie().getTitle() +"\t"+ String.valueOf(thisAmount) + "\n";
-		   totalAmount += thisAmount;
+		   result+= "\t" + each.getMovie().getTitle() +"\t"+ String.valueOf(each.getCharge()) + "\n";
+		   totalAmount += each.getCharge();
 	   }
 	   
 	   // add footer lines
